d050e1309adae6cb3c1165882840199f4006e188
Silverstreak101 <94749697+Silverstreak101@users.noreply.github.com>
2024-06-02 22:36:45 -0400

    Update Paddle.java

--- Paddle.java
+++ Paddle.java
@@ -1,71 +1,74 @@
 
+
+
 import java.awt.*;
 import java.awt.event.*;
 
 public class Paddle extends Rectangle {
 
-	private int yVelocity;
-	private int up, down; // key codes for the paddle going up and down, respectively
+	public int xVelocity;
+	public final int SPEED = 8; // speed of paddle
+	public static final int HEIGHT = 5, WIDTH = GamePanel.GAME_WIDTH/ 10; // dimensions of paddle
 
-	public final int SPEED = 8; // movement speed of paddle
-	public static final int LENGTH = 90; // vertical length of paddle
-	public static final int WIDTH = 7; // width (or thickness) of paddle
-
-	// constructor creates paddle at given location with given dimensions
-	// and specified controls for moving up and down
-	public Paddle(int x, int y, int u, int d) {
-		super(x, y, WIDTH, LENGTH);
-		up = u;
-		down = d;
+	public Paddle(int x, int y) {
+		super(x, y, WIDTH, HEIGHT);
+	
 	}
 
-	// called from GamePanel when any keyboard input is detected
-	// updates the direction of the ball based on user input
+	// checks for specific key input for each paddle
 	public void keyPressed(KeyEvent e) {
 
-		if (e.getKeyCode() == up) {
-			setYDirection(SPEED * -1);
-			move();
-		}
 
-		if (e.getKeyCode() == down) {
-			setYDirection(SPEED);
-			move();
-		}
+		// second paddle only moves via up and down arrow keys
+		
+			if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+				setXDirection(SPEED * -1);
+				move();
+			}
+
+			if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				setXDirection(SPEED);
+				move();
+			}
+		
+
 	}
 
-	// called from GamePanel when any key is released (no longer being pressed down)
-	// Makes the paddle stop moving in that direction
+	// makes paddles stop moving after specific keys are released
 	public void keyReleased(KeyEvent e) {
-		if (e.getKeyCode() == up || e.getKeyCode() == down) {
-			setYDirection(0);
-			move();
-		}
+
+		
+			if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+				setXDirection(0);
+				move();
+			}
+
+			if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+				setXDirection(0);
+				move();
+			}
+		
 	}
 
-	// called whenever the movement of the ball changes in the y-direction (up/down)
-	public void setYDirection(int yDirection) {
-		yVelocity = yDirection;
+	
+//called when movement of the paddle changes in the x-direction 
+	public void setXDirection(int xDirection) {
+		xVelocity = xDirection;
 	}
 
-	// called frequently from both PlayerBall class and GamePanel class
-	// updates the current location of the ball
+	// position of paddle updated according to velocity
 	public void move() {
-		y = y + yVelocity;
+		x = x + xVelocity;
 	}
 
-	// resets the position of the paddle to the specified location
-	// makes the paddle stationary
-	public void reset(int x, int y) {
-		this.x = x;
-		this.y = y;
-		yVelocity = 0;
-	}
-
-	// draws the current location of the paddle to the screen
+	// left paddle appears blue and right paddle appears red
 	public void draw(Graphics g) {
-		g.setColor(Color.white);
-		g.fillRect(x, y, WIDTH, LENGTH);
+		
+			g.setColor(Color.blue);
+	
+
+		g.fillRect(x, y, WIDTH, HEIGHT);
+
 	}
 
 }
