336538103cc672068720088c941af8180e021878
hanDynasty0 <142040454+hanDynasty0@users.noreply.github.com>
2024-06-03 14:40:52 -0400

    Update Brick.java

--- Brick.java
+++ Brick.java
@@ -1,21 +1,21 @@
 import java.awt.*;
 
 public class Brick extends Rectangle{
-	public static final int LENGTH = 100;
-	public static final int WIDTH = 50;
+	public static final int WIDTH = 100;
+	public static final int HEIGHT = 50;
 	
 	boolean hasPowerUp;
 	
 	Color color;
 	
 	public Brick(int x, int y, boolean hasPower, Color col) {
-		super(x,y);
+		super(x,y, WIDTH, HEIGHT);
 		hasPowerUp = hasPower;
 		color = col;
 	}
 	
 	public void draw(Graphics g) {
 		g.setColor(color);
-		g.fillRect(x, y, LENGTH, WIDTH);
+		g.fillRect(x, y, WIDTH, HEIGHT);
 	}
 }
