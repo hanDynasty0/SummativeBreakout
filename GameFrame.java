6e190c1e19abc3b2bd30afc6d5357d963903957a
Silverstreak101 <94749697+Silverstreak101@users.noreply.github.com>
2024-06-02 22:34:21 -0400

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -1,22 +1,26 @@
+/* 
+ * Author: Hazel Bains
+ * Date: May 21
+ * Description: GameFrame creates frame for running game
+ */
 
 import java.awt.*;
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
 
-	private GamePanel panel;
+        GamePanel panel;
 
-	// Constructor starts the game and sets properties for the game window
-	public GameFrame() {
-		panel = new GamePanel();
-		this.add(panel);
-		this.setTitle("PONG");
-		this.setResizable(false);
-		this.setBackground(Color.black);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.pack();
-		this.setVisible(true);
-		this.setLocationRelativeTo(null);
-	}
+        public GameFrame() {
+                panel = new GamePanel();
+                this.add(panel);
+                this.setTitle("Breakout");
+                this.setResizable(false); // frame will not change in size
+                this.setBackground(Color.black);
+                this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                this.pack();
+                this.setVisible(true);
+                this.setLocationRelativeTo(null);
+        }
 
 }
