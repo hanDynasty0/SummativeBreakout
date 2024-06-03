3621efb1dc63f338e9fb0102508b9b4f892cc15d
hanDynasty0 <142040454+hanDynasty0@users.noreply.github.com>
2024-06-02 21:06:07 -0400

    Update Brick.java
    Added methods, can change the length and width if necessary

--- Brick.java
+++ Brick.java
@@ -1,4 +1,21 @@
+import java.awt.*;
 
-public class Brick {
-
+public class Brick extends Rectangle{
+	public static final int LENGTH = 100;
+	public static final int WIDTH = 50;
+	
+	boolean hasPowerUp;
+	
+	Color color;
+	
+	public Brick(int x, int y, boolean hasPower, Color col) {
+		super(x,y);
+		hasPowerUp = hasPower;
+		color = col;
+	}
+	
+	public void draw(Graphics g) {
+		g.setColor(color);
+		g.fillRect(x, y, LENGTH, WIDTH);
+	}
 }
