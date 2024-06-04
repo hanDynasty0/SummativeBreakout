5574792e0480a1692a5bdb71f70423a3435c23ef
hanDynasty0 <142040454+hanDynasty0@users.noreply.github.com>
2024-06-03 23:24:50 -0400

    Update Brick.java

--- Brick.java
+++ Brick.java
@@ -1,3 +1,9 @@
+/* 
+ * Author: Han Fang and Hazel Bains
+ * Date: June 4
+ * Description: Brick class manages the display and position of the bricks
+ */
+
 import java.awt.*;
 
 public class Brick extends Rectangle{
@@ -7,13 +13,15 @@ public class Brick extends Rectangle{
 	boolean hasPowerUp;
 	
 	Color color;
-	
+
+	// constructor creates a brick at given location with given dimensions
 	public Brick(int x, int y, boolean hasPower, Color col) {
 		super(x,y, WIDTH, HEIGHT);
 		hasPowerUp = hasPower;
 		color = col;
 	}
-	
+
+	// appearance and position of the brick
 	public void draw(Graphics g) {
 		g.setColor(color);
 		g.fillRect(x, y, WIDTH, HEIGHT);
