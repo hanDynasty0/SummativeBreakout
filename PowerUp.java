bc9c5d572f45a44218ed335cf4baa920d596ebbb
hanDynasty0 <142040454+hanDynasty0@users.noreply.github.com>
2024-06-09 00:24:22 -0400

    Update PowerUp.java

--- PowerUp.java
+++ PowerUp.java
@@ -1,7 +1,7 @@
 /* 
  * Author: Han Fang and Hazel Bains
  * Date: June 11
- * Description: PowerUp class manages the display, position and velocity of the power 
+ * Description: PowerUp class manages the display, position and velocity of the power up
  */
 
 import java.awt.*;
@@ -9,8 +9,8 @@ import java.awt.*;
 public class PowerUp extends Rectangle{
 	public static final Color COLORS[] = {Color.yellow, Color.white};
 	
-	public static final int WIDTH = 40;
-	public static final int HEIGHT = 20;
+	public static final int WIDTH = 60;
+	public static final int HEIGHT = 15;
 	public static final int SPEED = 2;
 	
 	public Color color;
@@ -27,6 +27,20 @@ public class PowerUp extends Rectangle{
 	
 	public void draw(Graphics g) {
 		g.setColor(color);
-		g.fillRect(x, y, WIDTH, HEIGHT);
+		g.fillRect(x+WIDTH/4, y, WIDTH/2, HEIGHT);
+		g.fillOval(x, y, WIDTH/2, HEIGHT);
+		g.fillOval(x+WIDTH/2, y, WIDTH/2, HEIGHT);
+		
+		g.setFont(new Font("Consolas", Font.BOLD, 15));
+		g.setColor(Color.black);
+		if(color == Color.yellow) {
+			g.drawString("thru", x+WIDTH/4, y+HEIGHT-1);
+		}
+		else if(color == Color.white) {
+			g.drawString("+L", x+WIDTH/3, y+HEIGHT-1);
+		}
+		else if(color == Color.pink) {
+			g.drawString("-S", x+WIDTH/3, y+HEIGHT-1);
+		}
 	}
 }
